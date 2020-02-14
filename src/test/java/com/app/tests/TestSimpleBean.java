0d3e854d2ac30d6d0c384bbf8007d78342717bdc
KartikSudarshan <kartiksudd@gmail.com>
2020-02-14 12:17:10 +0530

    Simple Conversion of Java Oject to JSON and viceversa

---
+++ src/test/java/com/app/tests/TestSimpleBean.java
@@ -0,0 +1,34 @@
+package com.app.tests;
+
+import java.util.Date;
+
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+
+import com.app.beans.User;
+
+public class TestSimpleBean {
+
+	public static void main(String[]  args) {
+		//Create an instance of User
+		User user=new User();
+		user.setId(123);
+		user.setEmail("test@gmail.com");
+		user.setDate(new Date(456789));
+		user.setName("JsonTester");
+		
+		//Create JsonB instance from JsonbBuilder
+		Jsonb jsonb=JsonbBuilder.create();
+		
+		//Use Jsonb.toJson() to convert the user object into JSON representation
+		String result =jsonb.toJson(user);
+		System.out.println(result);
+		
+		//Use Jsonb.fromJson() to convert the JSON String back to a User Object
+		Jsonb jsonb1=JsonbBuilder.create();
+		User user2=jsonb1.fromJson(result, User.class);
+		System.out.println("user 2 details are: \n"+user2);
+		
+	}
+	
+}
